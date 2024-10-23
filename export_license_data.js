370b9f39fb83411bb87bc873ece05c413e47ea4b
Harjot Gill <harjotgill2025@gmail.com>
2024-10-23 14:33:08 -0700

    Added structure for license export function

--- export_license_data.js
+++ export_license_data.js
@@ -11,6 +11,49 @@
 
 (function() {
     'use strict';
+    // Function to add the recurring service booking options
+    function addButtons() {
+        // Select the target element where buttons will be added
+        var lowerRow = document.querySelector('.modal-body');
 
-    // Your code here...
+        if (lowerRow) {
+            // query the third child of the lowerRow which is a div with class 'row'
+            var row = lowerRow.children[2];
+
+            if (row) {
+                // Check if the button already exists to avoid adding it multiple times
+                if (!row.querySelector('.btn-primary')) {
+                    // Create the button element
+                    var button = document.createElement('button');
+                    button.innerHTML = 'Recurring Service Booking';
+                    button.className = 'btn btn-primary';
+                    button.style.marginLeft = '10px';
+                    button.style.marginRight = '10px';
+
+                    // Add the button to the row
+                    row.appendChild(button);
+                }
+            }
+        }
+    }
+
+    // Create a MutationObserver to watch for changes in the DOM
+    const observer = new MutationObserver(() => {
+        // Check if the target (form) element is now loaded in the DOM
+        
+        /// TODO: Find the element you want to observe and insert into the querySelector below
+        // if (document.querySelector("INSERT ELEMENT TO OBSERVE HERE")) {
+        //     //wait for 0.1 seconds
+        //     setTimeout(addButtons, 100);
+        // }
+
+
+    });
+
+    // Start observing the document for changes in the DOM
+    observer.observe(document, {
+        childList: true,
+        subtree: true
+    });
+
 })();
