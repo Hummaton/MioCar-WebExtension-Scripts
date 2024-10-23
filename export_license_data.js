512332ba0116653228986708c6226e91ba17cef6
Harjot Gill <harjotgill2025@gmail.com>
2024-10-23 14:28:54 -0700

    Added boiler code for license export function

---
+++ export_license_data.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Export License Data
+// @namespace    http://tampermonkey.net/
+// @version      2024-10-23
+// @description  try to take over the world!
+// @author       You
+// @match        https://admin.share.car/communities/671/customers/members/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=share.car
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
