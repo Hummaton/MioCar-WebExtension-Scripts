f89ffb92877d96c2f711e217b3ce610019f90285
Harjot Gill <harjotgill2025@gmail.com>
2024-11-23 18:28:11 -0800

    Modified recurring service booking functionality to be simplier and cleaner. Button and input is on the same form

--- booking_export.js
+++ booking_export.js
@@ -2,7 +2,6 @@
 // @name         Booking Report Export 
 // @namespace    http://tampermonkey.net/
 // @version      2024-11-22
-// @description  try to take over the world!
 // @author       You
 // @match        https://admin.share.car/reports
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=share.car
--- serviceBookingRepeat.js
+++ serviceBookingRepeat.js
@@ -12,41 +12,100 @@
 (function() {
     'use strict';
 
-    function addButtons() {
-        const lowerRow = document.querySelector('.modal-footer');
+    function addRepeatIntervalMenu() {
+        const serviceBookingMenuRow = document.querySelector("body > sc-app-root > sc-service-booking-modal > div.modal.note-modal.fade.in > div > div > form > div.modal-body > div:nth-child(3)")
 
         // Check if the button already exists
-        if (!document.getElementById('recurring-booking-button') && lowerRow) {
-            const newButton = document.createElement('button');
+        if (!document.getElementById('recurring-booking-element') && serviceBookingMenuRow) {
+            const recurringBookingDiv = document.createElement('div');
+            recurringBookingDiv.id = 'recurring-booking-element';
+            recurringBookingDiv.className = 'col-md-6';
 
-            // Add the style attributes to the button
-            newButton.className = 'btn btn-success';
-            newButton.id = 'recurring-booking-button';
-            newButton.style.backgroundColor = 'maroon';
-            newButton.textContent = 'Recurring Booking';
+            const label = document.createElement('label');
+            label.setAttribute('for', 'repeatInterval');
+            label.textContent = 'Repeat Interval';
 
-            lowerRow.appendChild(newButton);
+            const selectDiv = document.createElement('div');
+            selectDiv.className = 'custom-select form-control-md';
+
+            const select = document.createElement('select');
+            select.id = 'repeatInterval';
+            select.name = 'repeatInterval';
+            select.className = 'ng-untouched ng-pristine ng-valid';
+
+            const optionDefault = document.createElement('option');
+            optionDefault.selected = true;
+            optionDefault.value = 'undefined';
+            optionDefault.textContent = 'Select...';
+
+            const options = ['Daily', 'Weekly', 'Bi-Weekly', 'Monthly', 'Yearly'];
+            options.forEach(optionText => {
+                const option = document.createElement('option');
+                option.textContent = optionText;
+                option.value = optionText.toLowerCase().replace(/ /g, '-');
+                select.appendChild(option);
+            });
+
+            select.appendChild(optionDefault);
+            selectDiv.appendChild(select);
+
+            recurringBookingDiv.appendChild(label);
+            recurringBookingDiv.appendChild(selectDiv);
+
+            
+
+            if (serviceBookingMenuRow) {
+                serviceBookingMenuRow.appendChild(recurringBookingDiv);
+            }
+    }
+}
+
+
+function addIntervalStopDate() {
+    const serviceBookingMenuRow = document.querySelector("body > sc-app-root > sc-service-booking-modal > div.modal.note-modal.fade.in > div > div > form > div.modal-body > div:nth-child(3)")
+
+    // Check if the button already exists
+    if (!document.getElementById('recurring-booking-end-date') && serviceBookingMenuRow) {
+        const endDateDiv = document.createElement('div');
+        endDateDiv.id = 'recurring-booking-end-date';
+        endDateDiv.className = 'col-md-6';
+
+        const formGroupDiv = document.createElement('div');
+        formGroupDiv.className = 'form-group';
+        formGroupDiv.setAttribute('scvalidationstyle', 'endDate');
+
+        const label = document.createElement('label');
+        label.setAttribute('for', 'input-end-date');
+        label.textContent = 'Repeat End Date';
+
+        const input = document.createElement('input');
+        input.type = 'date';
+        input.id = 'input-end-date';
+        input.name = 'endDate';
+        input.placeholder = 'Select a date';
+        input.className = 'form-control ng-untouched ng-pristine ng-valid';
+
+        formGroupDiv.appendChild(label);
+        formGroupDiv.appendChild(input);
+        endDateDiv.appendChild(formGroupDiv);
+
+        if (serviceBookingMenuRow) {
+            serviceBookingMenuRow.appendChild(endDateDiv);
         }
     }
+}
 
-    function changeSingleBookingButtonName() {
-        const lowerRow = document.querySelector('.modal-footer');
-        // Check if the button already exists
-        if (lowerRow && lowerRow.children[1]) {
-            // Change the button text to 'Single Booking'
-            lowerRow.children[1].innerHTML = 'Single Booking';
-        }
-    }
+
 
     function main() {
-        alert("hello")
         const observer = new MutationObserver(() => {
+            const serviceBookingRow = document.querySelector("body > sc-app-root > sc-service-booking-modal > div.modal.note-modal.fade.in > div > div > form > div.modal-body > div:nth-child(3)")
             // Check if the target (modal footer) element is now loaded in the DOM
-            if (document.querySelector(".modal-footer")) {
+            if (serviceBookingRow) {
                 // Wait for 100ms
                 setTimeout(() => {
-                    changeSingleBookingButtonName();
-                    addButtons();
+                    addRepeatIntervalMenu();
+                    addIntervalStopDate();
                 }, 100);
             }
         });
