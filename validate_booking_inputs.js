93f252433e9515e2560dffd186c213974f24a128
KyleGal <kylegmg@gmail.com>
2025-02-04 12:06:58 -0800

    Combined replacing check availability button and check availability functionality

--- validate_booking_inputs.js
+++ validate_booking_inputs.js
@@ -305,12 +305,28 @@
 
     // MAIN FUNCTION
     const booking_interval_id = setInterval(() => {
-        // Check if the "Check Availability" button is now in the DOM
-        var check_avail_button = document.querySelector("#new-check-availability-button");
+        // Check if Repeat Interval and Repeat End Date are BOTH filled before replacing "Check Availability" button
+        var curr_check_avail_button = document.querySelector("body > sc-app-root > sc-service-booking-modal > div.modal.note-modal.fade.in > div > div > form > div.modal-footer > button.btn.btn-blue");
+        var repeat_interval_select_element = document.querySelector("#repeatInterval");
+        var repeat_end_date_input_element = document.querySelector("#input-end-date");
+        var new_check_avail_element = document.querySelector("#new-check-availability-button");
 
-        if (check_avail_button) {
+        if (curr_check_avail_button && !new_check_avail_element && repeat_interval_select_element.value != 'undefined' && repeat_end_date_input_element.value != '') {
+            console.log("Removed old Check Availability");
+            curr_check_avail_button.style.display = 'none';
+            
+            // Create New Check Availability Button
+            const new_check_avail_button = document.createElement('button');
+            new_check_avail_button.type = 'button';
+            new_check_avail_button.id = 'new-check-availability-button';
+            new_check_avail_button.className = 'btn btn-blue';
+            new_check_avail_button.innerHTML = 'New Check Availability';
+
+            const check_availability_parent_div = document.querySelector("body > sc-app-root > sc-service-booking-modal > div.modal.note-modal.fade.in > div > div > form > div.modal-footer");
+            check_availability_parent_div.insertBefore(new_check_avail_button, check_availability_parent_div.children[1]);
+
             // Add the click event listener to the button
-            check_avail_button.addEventListener("click", function () {
+            new_check_avail_button.addEventListener("click", function () {
                 console.log("CHECKING AVAILABILITY");
                 const payload = validate_inputs();
                 check_availability(payload);
