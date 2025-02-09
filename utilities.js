f44774d28674463c174750a2c8aa213138c0f7d8
Harjot Gill <harjotgill2025@gmail.com>
2025-02-08 17:35:49 -0800

    Update utilities script

--- utilities.js
+++ utilities.js
@@ -42,13 +42,29 @@
         return null; // Return null if no value is found
     }
 
-
     function criticalError(error) {
         alert('Error has occurred. Check the console for more information.');
         console.error('Critical error:', error);
 
         //TODO: Implement an API Call to Google sheet to log the error
         
+    }
+
+    /* 'date' is the js Date object */
+    function convertDatetimeToString(date) {
+        if (date instanceof Date) {
+            const year = date.getFullYear();
+            const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+            const day = String(date.getDate()).padStart(2, '0');
+            const hours = String(date.getHours()).padStart(2, '0');
+            const minutes = String(date.getMinutes()).padStart(2, '0');
+            const seconds = String(date.getSeconds()).padStart(2, '0');
+
+            return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+        } else {
+            console.error("Not a valid Date object");
+        }
+        return null;
     }
 
     // Make the retrieveAccessToken function available globally but protect against overwriting
@@ -73,5 +89,17 @@
         console.log('criticalError function is now globally available.');
     } else {
         console.warn('criticalError is already defined and will not be overwritten.');
+    }
+
+    // Make the convertDatetimeToString function available globally but protect against overwriting
+    if (!window.convertDatetimeToString) {
+        Object.defineProperty(window, 'convertDatetimeToString', {
+            value: convertDatetimeToString,
+            writable: false, // Prevent overwriting
+            configurable: false, // Prevent redefinition
+        });
+        console.log('convertDatetimeToString function is now globally available.');
+    } else {
+        console.warn('convertDatetimeToString is already defined and will not be overwritten.');
     }
 })();
