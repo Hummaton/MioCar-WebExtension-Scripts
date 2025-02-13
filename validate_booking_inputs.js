1bb98ba3caa5e6fc605118a594965afaaaf97fcc
KyleGal <kylegmg@gmail.com>
2025-02-13 13:05:35 -0800

    Complete check availability and create booking result displays. Need to test further and clean up code for readability purposes.

--- validate_booking_inputs.js
+++ validate_booking_inputs.js
@@ -11,31 +11,31 @@
 // ==/UserScript==
 
 (function() {
-    'use strict'; 
+    'use strict';
     var vehicle_id_str;
 
     /* HTTP Responses */
     // The HTTP 201 Created successful response status code indicates that the HTTP request has led to the creation of a resource.
-    const CREATED = 201; 
-    
-    /* The HyperText Transfer Protocol (HTTP) 400 Bad Request response status code indicates that the server cannot or will not process the request due to 
-    something that is perceived to be a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing). */ 
-    const BAD_REQUEST = 400; 
+    const CREATED = 201;
+
+    /* The HyperText Transfer Protocol (HTTP) 400 Bad Request response status code indicates that the server cannot or will not process the request due to
+    something that is perceived to be a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing). */
+    const BAD_REQUEST = 400;
 
     // The HTTP 401 Unauthorized client error status response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.
-    const INVALID_TOKEN = 401; 
+    const INVALID_TOKEN = 401;
 
     // The HTTP 403 Forbidden client error status response code indicates that the server understood the request but refuses to authorize it.
-    const FORBIDDEN = 403; 
+    const FORBIDDEN = 403;
 
     // The HTTP 404 Not Found client error response code indicates that the server can't find the requested resource.
     const NOT_FOUND = 404;
-    
+
     // The HTTP 422 Unprocessable Content client error response status code indicates that the server understood the content type of the request content, and the syntax of the request content was correct, but it was unable to process the contained instructions.
-    const UNPROCESSABLE_CONTENT = 422; 
+    const UNPROCESSABLE_CONTENT = 422;
 
     // The HTTP 429 Too Many Requests response status code indicates the user has sent too many requests in a given amount of time ("rate limiting").
-    const TOO_MANY_REQUESTS = 429; 
+    const TOO_MANY_REQUESTS = 429;
 
     // The HTTP 500 Internal Server Error server error response code indicates that the server encountered an unexpected condition that prevented it from fulfilling the request.
     const INTERNAL_SERVER_ERROR = 500;
@@ -45,9 +45,9 @@
 
     let DELAY_AMMOUNT = 100; // 0.1s wait time between making API requests to prevent rate limiting, server overload, packet loss, etc.
 
-    // POST arguments for API call 
+    // POST arguments for API call
     /************* FILL IN FOR PRODUCTION SCRIPT  */
-    const url = API_ENDPOINT; 
+    const url = API_ENDPOINT;
     const referer = POST_HEADERS_REFERER;
     /************* FILL IN FOR PRODUCTION SCRIPT  */
 
@@ -66,7 +66,7 @@
 
         return headers;
     }
-    
+
     function repeatIntervalToInt(repeat_interval) {
         // ['Daily', 'Weekly', 'Bi-Weekly', 'Monthly', 'Yearly']
         var repeat_interval_int = 0;
@@ -100,7 +100,7 @@
         var repeat_interval_element = document.getElementById("repeatInterval");
         var input_end_date_element = document.getElementById("input-end-date");
         if (purpose_element.value == 'undefined') {
-            alert("Select a Booking Purpose"); 
+            alert("Select a Booking Purpose");
             return null;
         }
 
@@ -133,7 +133,7 @@
         // Create payload
         const pickup_datetime_string = convertDatetimeToString(pickup_datetime_obj);
         const dropoff_datetime_string = convertDatetimeToString(dropoff_datetime_obj);
-        const type = "service"; 
+        const type = "service";
         const purpose = purpose_element.value;
         const dry_run = true; // Dry run paramter required to check availability
         const repeat_interval = repeatIntervalToInt(repeat_interval_element.value);
@@ -146,7 +146,6 @@
         const vehicle_id_element = document.querySelector("body > sc-app-root > sc-app-root > div:nth-child(2) > section > div > div > div:nth-child(1) > main > ng-component > div > section:nth-child(1) > form > header > div > div.col-md-7 > div > div.title-data-item.strong.ng-star-inserted");
         vehicle_id_str = vehicle_id_element.innerHTML;
         var vehicle_id_int = parseInt(vehicle_id_str.split(" ")[1]);
-        console.log("VEHICLE ID", vehicle_id_int);
 
         const payload = {
             'pickUpDatetime': pickup_datetime_string,
@@ -171,8 +170,8 @@
                 break;
             case (INVALID_TOKEN || FORBIDDEN):
                 error_string = "Invalid Token or Forbidden Access for recurring booking script\n API Call Headers: " + headers + "\nAPI Call Body: " + body + "\nResponse: " + response;
-                /* Since we plan to retrieve the token from the browser's localStorage right before making the API call, 
-                we can assume that we are getting the most up to date key. Highly unlikely that reattempting the request will work. 
+                /* Since we plan to retrieve the token from the browser's localStorage right before making the API call,
+                we can assume that we are getting the most up to date key. Highly unlikely that reattempting the request will work.
                 so we will not retry the request. */
                 break;
             case NOT_FOUND:
@@ -205,34 +204,88 @@
                 break;
             default:
                 console.log('An unexpected error occurred: ', response.status);
-                console.log('Response:', response); 
+                console.log('Response:', response);
                 alert('An unexpected error occurred, please try again later');
                 error_string = "Unexpected error for recurring booking script\n API Call Headers: " + headers + "\nAPI Call Body: " + body + "\nResponse: " + response;
-                break;           
+                break;
         }
 
-        //API Call to Google sheet to log the error (future development) 
+        //API Call to Google sheet to log the error (future development)
         criticalError(error_string);
-     
     }
 
-    function processBookingBadResponse() { // TODO: Handle error responses for booking
-        return;
+    function createRedMessage(str) {
+        const message_container_div = document.querySelector("body > sc-app-root > sc-service-booking-modal > div.modal.note-modal.fade.in > div > div > form > div.modal-body > div:nth-child(4) > div");
+        message_container_div.style.display = "flex";
+        message_container_div.style.gap = "10px";
+        message_container_div.style.justifyContent = "center";
+        var message_div = document.querySelector("#red-message");
+
+        if (message_div) {
+            message_div.innerHTML = str;
+        } else {
+            message_div = document.createElement('div');
+            message_div.id = 'red-message';
+            message_div.className = 'alert alert-danger ng-star-inserted';
+            message_div.innerHTML = str;
+
+            message_div.style.flex = "1";
+
+            message_container_div.appendChild(message_div);
+        }
+    }
+
+    function createGreenMessage(str) {
+        const message_container_div = document.querySelector("body > sc-app-root > sc-service-booking-modal > div.modal.note-modal.fade.in > div > div > form > div.modal-body > div:nth-child(4) > div");
+        message_container_div.style.display = "flex";
+        message_container_div.style.gap = "10px";
+        message_container_div.style.justifyContent = "center";
+        var message_div = document.querySelector("#green-message");
+
+        if (message_div) {
+            message_div.innerHTML = str;
+        } else {
+            message_div = document.createElement('div');
+            message_div.id = 'green-message';
+            message_div.className = 'alert alert-success ng-star-inserted';
+            message_div.innerHTML = str;
+
+            message_div.style.flex = "1";
+
+            message_container_div.appendChild(message_div);
+        }
+    }
+
+    function removeMessages() {
+        var green_message_div = document.querySelector("#green-message");
+        if (green_message_div) {
+            green_message_div.remove();
+        }
+
+        var red_message_div = document.querySelector("#red-message");
+        if (red_message_div) {
+            red_message_div.remove();
+        }
     }
 
     async function checkAvailability(payload) {
+        if (payload == null) {
+            alert("Invalid payload");
+            return;
+        }
+
         const pickup_datetime_obj = new Date(payload.pickUpDatetime);
         const dropoff_datetime_obj = new Date(payload.dropOffDatetime);
         const repeat_end_datetime_obj = new Date(payload.endDatetime);
         const repeat_interval_int = payload['repeat-interval'];
         console.log("Checking availability of intervals of " + repeat_interval_int + " starting from " + convertDatetimeToString(pickup_datetime_obj) + " to " + convertDatetimeToString(repeat_end_datetime_obj));
 
-        var bad_dates = [];
-        var good_date_payloads = [];
+        var invalid_dates = [];
+        var valid_date_payloads = [];
         var curr_pickup_datetime_obj = pickup_datetime_obj;
         var curr_dropoff_datetime_obj = dropoff_datetime_obj;
 
-        // Incrementally check availability for each date in the range 
+        // Incrementally check availability for each date in the range
         while (curr_pickup_datetime_obj <= repeat_end_datetime_obj) {
             const request_payload = {
                 'pickUpDatetime': convertDatetimeToString(curr_pickup_datetime_obj),
@@ -263,20 +316,20 @@
             }
 
             // Handle POST responses
-            if (!(response.status == CREATED)) {                
-                processBadResponse(response, bad_dates, requestHeaders, requestBody, convertDatetimeToString(curr_pickup_datetime_obj));
+            if (!(response.status == CREATED)) {
+                processBadResponse(response, invalid_dates, requestHeaders, requestBody, convertDatetimeToString(curr_pickup_datetime_obj));
             } else {
                 // Create payloads for valid dates to use for booking
-                const good_date_payload = {
+                const valid_date_payload = {
                     'pickUpDatetime': convertDatetimeToString(curr_pickup_datetime_obj),
                     'dropOffDatetime': convertDatetimeToString(curr_dropoff_datetime_obj),
                     'type': payload.type,
                     'vehicle': payload.vehicle,
                     'purpose': payload.purpose
                 };
-                good_date_payloads.push(good_date_payload);
+                valid_date_payloads.push(valid_date_payload);
             }
-            
+
             // Increment pickup and dropoff datetimes by the repeat interval
             curr_pickup_datetime_obj.setDate(curr_pickup_datetime_obj.getDate() + repeat_interval_int);
             curr_dropoff_datetime_obj.setDate(curr_dropoff_datetime_obj.getDate() + repeat_interval_int);
@@ -284,42 +337,71 @@
             await new Promise(resolve => setTimeout(resolve, DELAY_AMMOUNT)); // Pause for specified time to prevent rate limiting
         }
 
-        
-        if (good_date_payloads.length == 0) {
-            alert("No dates available to book for the given range. Please choose another date"); // TODO: Display nice response to user
-        } else if (bad_dates.length == 0) {
-            alert("All dates available"); // TODO: Display nice response to user
-        } else {
-            var output = "Bad Dates:\n";
-            for (let i = 0; i < bad_dates.length; i++) {
-                output = output + bad_dates[i] + "\n";
-            }
-            alert(output); // TODO: Display nice response to user
-        }
-        console.log("Valid Dates", good_date_payloads);
-        console.log("Invalid Dates", bad_dates);
+        console.log("DISPLAY DATES");
+        // DISPLAY DATES
+        if (valid_date_payloads.length == 0) {
+            removeMessages();
+            const only_invalid_dates_msg = "No dates available to book for the given range. Please choose another date.";
+            createRedMessage(only_invalid_dates_msg);
+        } else if (invalid_dates.length == 0) {
+            removeMessages();
 
-        return good_date_payloads;
+            var only_valid_dates_msg = "All dates available:<br />";
+
+            for (let i = 0; i < valid_date_payloads.length; i++) {
+                var valid_date = new Date(valid_date_payloads[i].pickUpDatetime);
+                valid_date = valid_date.toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" });
+
+                only_valid_dates_msg = only_valid_dates_msg + valid_date + "<br />";
+            }
+
+            createGreenMessage(only_valid_dates_msg);
+        } else {
+            removeMessages();
+
+            var valid_dates_msg = "Available Dates:<br />";
+            for (let i = 0; i < valid_date_payloads.length; i++) {
+                var valid_date = new Date(valid_date_payloads[i].pickUpDatetime);
+                valid_date = valid_date.toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" });
+
+                valid_dates_msg = valid_dates_msg + valid_date + "<br />";
+            }
+            createGreenMessage(valid_dates_msg);
+
+
+            var invalid_dates_msg = "Unavailable Dates:<br />";
+            for (let i = 0; i < invalid_dates.length; i++) {
+                var invalid_date = new Date(invalid_dates[i]);
+                invalid_date = invalid_date.toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" });
+
+                invalid_dates_msg = invalid_dates_msg + invalid_date + "<br />";
+            }
+            createRedMessage(invalid_dates_msg);
+        }
+        console.log("Valid Dates", valid_date_payloads);
+        console.log("Invalid Dates", invalid_dates);
+
+        return valid_date_payloads;
     }
 
     // BOOKING FUNCTIONALITY
-    // good_date_payloads is a list of payloads for valid dates to book'
-    async function bookAvailableDates(good_date_payloads) {
+    // valid_date_payloads is a list of payloads for valid dates to book'
+    async function bookAvailableDates(valid_date_payloads) {
 
-        if (good_date_payloads.length == 0 || good_date_payloads == null) {
+        if (valid_date_payloads.length == 0 || valid_date_payloads == null) {
             alert("No valid dates to book");
             return;
         }
 
-        for (let i = 0; i < good_date_payloads.length; i++) {
+        for (let i = 0; i < valid_date_payloads.length; i++) {
             // Send POST to server
             var response;
             try {
-                console.log('Attempting to send POST request for booking creation... on date:', good_date_payloads[i].pickUpDatetime);
+                console.log('Attempting to send POST request for booking creation... on date:', valid_date_payloads[i].pickUpDatetime);
                 response = await fetch(url, {
                     method: 'POST',
                     headers: getPostHeader(),
-                    body: JSON.stringify(good_date_payloads[i])
+                    body: JSON.stringify(valid_date_payloads[i])
                 });
                 console.log('Status Code:', response.status);
             } catch (error) {
@@ -328,7 +410,6 @@
 
             // Handle POST responses
             if (!(response.status == CREATED)) {
-                // TODO: Check for 4** errors and handle below
                 processBookingBadResponse();
             }
 
@@ -336,27 +417,34 @@
         }
 
         alert("All bookings attempted");
-        // TODO: ALERT USER OF ALL BOOKINGS MADE AND IF ANY WERE UNSUCCESSFUL
 
+        // Create message displaying booked dates
+        removeMessages();
+        var only_valid_dates_msg = "Booked the following dates:<br />";
+        for (let i = 0; i < valid_date_payloads.length; i++) {
+            var valid_date = new Date(valid_date_payloads[i].pickUpDatetime);
+            valid_date = valid_date.toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" });
+
+            only_valid_dates_msg = only_valid_dates_msg + valid_date + "<br />";
+        }
+        createGreenMessage(only_valid_dates_msg);
     }
 
-    
+
     // MAIN FUNCTION
     const booking_interval_id = setInterval(() => {
-        // Replacing 'Check Availability Button' and add functionality
+        var checked_input;
+        var valid_date_payloads;
+
         var curr_check_avail_button = document.querySelector("body > sc-app-root > sc-service-booking-modal > div.modal.note-modal.fade.in > div > div > form > div.modal-footer > button.btn.btn-blue");
         var repeat_interval_select_element = document.querySelector("#repeatInterval");
         var repeat_end_date_input_element = document.querySelector("#input-end-date");
         var new_check_avail_element = document.querySelector("#new-check-availability-button");
 
-        var checked_input;
-        var good_date_payloads;
-
-
         if (curr_check_avail_button && !new_check_avail_element && repeat_interval_select_element.value != 'undefined' && repeat_end_date_input_element.value != '') {
-            console.log("Removed old Check Availability");
+            console.log("Remove Check Availability");
             curr_check_avail_button.style.display = 'none';
-            
+
             // Create New Check Availability Button
             const new_check_avail_button = document.createElement('button');
             new_check_avail_button.type = 'button';
@@ -372,7 +460,7 @@
                 console.log("CHECKING AVAILABILITY");
                 checked_input = validateInputs();
                 if (checked_input) {
-                    good_date_payloads = await checkAvailability(checked_input);
+                    valid_date_payloads = await checkAvailability(checked_input);
                 }
             });
 
@@ -399,18 +487,18 @@
 
             new_create_booking_button.addEventListener("click", async function () {
                 console.log("CREATE BOOKING");
-                
+
                 var form_changed = JSON.stringify(checked_input) === JSON.stringify(validateInputs()) ? false : true;
 
                 if (form_changed) {
                     alert('Form changed, recheck availability');
                 } else {
                     console.log('Form not changed, proceed to booking');
-                    console.log('Good Date Payloads:', good_date_payloads);
+                    console.log('Valid Date Payloads:', valid_date_payloads);
 
-                    bookAvailableDates(good_date_payloads);
+                    bookAvailableDates(valid_date_payloads);
                 }
-                
+
             });
 
             // Stop checking once the button is found and event listener is attached
