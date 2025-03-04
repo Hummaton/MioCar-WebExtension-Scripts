f94911db011bedd6d0502b3b0bb2964655140dc5
Harjot Gill <harjotgill2025@gmail.com>
2025-03-03 21:48:39 -0800

    Implemented valid trip filtering to keep bookings that were not cancelled. Cleaned up processMemberData function. Added global url to specifiy which api call to intercept

--- bookingExport.js
+++ bookingExport.js
@@ -6,15 +6,17 @@
 // @match        https://admin.share.car/reports
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=share.car
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/Hummaton/MioCar-WebExtension-Scripts/refs/heads/main/bookingExport.js
-// @downloadURL  https://raw.githubusercontent.com/Hummaton/MioCar-WebExtension-Scripts/refs/heads/main/bookingExport.js
-// @require      https://raw.githubusercontent.com/Hummaton/MioCar-WebExtension-Scripts/serviceBooking_dev/utilities.js
+// @require      https://raw.githubusercontent.com/Hummaton/MioCar-WebExtension-Scripts/main/utilities.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+
+    /************* FILL IN FOR PRODUCTION SCRIPT  */
+    const TARGET_URL = ""; // Target API endpoint
+    /************* FILL IN FOR PRODUCTION SCRIPT  */
 
     async function generateExcelSheet(data) {
         let date_from_element = document.querySelector("body > sc-app-root > sc-app-root > div:nth-child(2) > section > div > div > div:nth-child(1) > main > ng-component > div > section > header > div > div.col-md-8 > p > strong:nth-child(2) > sc-date-display:nth-child(1) > span");
@@ -52,6 +54,21 @@
     }
 
     async function processMemberData(data_arr) {
+
+        // Function to format time as "h:mmam/pm"
+        function formatTime(date) {
+            let hours = date.getHours();
+            const minutes = date.getMinutes().toString().padStart(2, '0');
+            const ampm = hours >= 12 ? "pm" : "am";
+            hours = hours % 12 || 12; // Convert to 12-hour format
+            return `${hours}:${minutes}${ampm}`;
+        }
+
+        // Function to format date as "M/D"
+        function formatDate(date) {
+            return `${date.getMonth() + 1}/${date.getDate()}`;
+        }
+
         console.log(data_arr);
 
         // Contains header for excel sheet
@@ -60,12 +77,17 @@
             "Requested Duration", "Actual Duration", "Miles Driven", "Vehicle Used", "Location", "Revenue",
             "Trip Purpose", "Notes"]];
         const items = data_arr._embedded.items;
+
         for (let i=0; i<items.length; i++) {
+
+            if (items[i].tripDistance === null) {
+                continue;
+            }
+
             // Separate member first and last name
             const member_name = items[i].memberName.split(" ");
             const member_first_name = member_name[0];
             const member_last_name = member_name[1];
-
 
             // Pickout Program Location
             var program_location;
@@ -80,7 +102,6 @@
                     console.log("Unknown program location");
             }
 
-
             // Calculate requested duration and actual duration
             const requested_duration = Math.abs(new Date(items[i].dropOffDatetime) - new Date(items[i].pickUpDatetime)) / (1000 * 60 * 60);
             const actual_duration = Math.abs(new Date(items[i].tripDropOffDatetime) - new Date(items[i].tripPickUpDatetime)) / (1000 * 60 * 60);
@@ -90,27 +111,12 @@
             const start_date = new Date(items[i].pickUpDatetime);
             const end_date = new Date(items[i].dropOffDatetime);
 
-            // Function to format time as "h:mmam/pm"
-            function formatTime(date) {
-                let hours = date.getHours();
-                const minutes = date.getMinutes().toString().padStart(2, '0');
-                const ampm = hours >= 12 ? "pm" : "am";
-                hours = hours % 12 || 12; // Convert to 12-hour format
-                return `${hours}:${minutes}${ampm}`;
-            }
-
-            // Function to format date as "M/D"
-            function formatDate(date) {
-                return `${date.getMonth() + 1}/${date.getDate()}`;
-            }
-
             const start_month_day = formatDate(start_date);
             const end_month_day = formatDate(end_date);
             const start_time = formatTime(start_date);
             const end_time = formatTime(end_date);
 
             const external_data_reference = `${start_month_day}-${end_month_day} from ${start_time}-${end_time}`;
-
 
             // get current date
             var today = new Date();
@@ -122,16 +128,6 @@
                 items[i].tripDistance, items[i].vehiclePlate, items[i].stationName, items[i].totalRevenue, "", ""
             ];
 
-            /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-             * TODO: Still need gather info on Date!, Survey Sent, Survey Completed, Applied Promo Code,     *
-             * Requested Duration!, Actual Duration!, and Trip Purpose to add to payload.                    *
-             *                                                                                               *
-             * For DATE: not sure if that means the current date when they enter the data or something else  *
-             * For ExternalDataReference: is this the booked time? (pickUpDatetime and dropOffDatetime)      *
-             * For Requested Duration: is this dropOffDatetime - pickUpDatetime?                             *
-             * For Actual Duration: is this tripDropOffDatetime - tripPickUpDatetime?                        *
-             * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
             member_bookings.push(payload);
         }
 
@@ -139,6 +135,7 @@
     }
 
     var data_response_arr;
+
     // Function to add the CSV export button
     function addButton() {
         // Select the target element where the button will be added
@@ -172,6 +169,9 @@
                 await generateExcelSheet(member_bookings);
                 const end = performance.now();
                 console.log(`Report Export took ${(end - start) / 1000} seconds`);
+                console.log("Member Bookings processed: ", member_bookings.length);
+                let time_saved = 160 * member_bookings.length;   //Each booking takes 160 seconds roughly to process manually
+                console.log("Time saved: ", time_saved);
             });
         }
     }
@@ -201,7 +201,7 @@
             });
         }
 
-        if (url_arg.includes(url)) {
+        if (url_arg.includes(TARGET_URL)) {
             this.addEventListener("load", function() {
                 console.log("[Tampermonkey] Intercepted XHR request:", {
                     method: method,
