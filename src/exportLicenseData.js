2ccd8321ed601910b9c1eee2c46f4c5a26ac0390
KyleGal <kylegmg@gmail.com>
2025-03-11 12:03:12 -0700

    Gather user data required for MVR Checker. Next steps would be to fill in the mvr checker automatically

--- src/exportLicenseData.js
+++ src/exportLicenseData.js
@@ -14,24 +14,38 @@
 (function() {
     'use strict';
 
-    // Function to format user data into a CSV file
-    function convertToCSV() {
-        // Gather user data
-        var userName = document.querySelector("body > sc-app-root > sc-app-root > div:nth-child(2) > section > div > div > div:nth-child(1) > main > ng-component > form > div > section:nth-child(1) > section > sc-personal-info-summary > div:nth-child(1) > div:nth-child(1) > div > span");
-        var userEmail = document.querySelector("body > sc-app-root > sc-app-root > div:nth-child(2) > section > div > div > div:nth-child(1) > main > ng-component > form > div > section:nth-child(1) > section > sc-personal-info-summary > div:nth-child(1) > div:nth-child(2) > div > span > a");
-        var userPhone = document.querySelector("body > sc-app-root > sc-app-root > div:nth-child(2) > section > div > div > div:nth-child(1) > main > ng-component > form > div > section:nth-child(1) > section > sc-personal-info-summary > div:nth-child(1) > div:nth-child(3) > div > sc-telephone-link > a > span");
-        var userDOB = document.querySelector("body > sc-app-root > sc-app-root > div:nth-child(2) > section > div > div > div:nth-child(1) > main > ng-component > form > div > section:nth-child(1) > section > sc-personal-info-summary > div:nth-child(2) > div.col-md-4 > div > sc-date-display > span");
-        var userAddress = document.querySelector("body > sc-app-root > sc-app-root > div:nth-child(2) > section > div > div > div:nth-child(1) > main > ng-component > form > div > section:nth-child(1) > section > sc-personal-info-summary > div:nth-child(2) > div.col-md-8 > div > span > a");
+    // Function to format user data into payload for MVR Checker
+    function processUserInfo() {
+        // Process user info into what mvr checker requires
+        var user_DOB = document.querySelector("body > sc-app-root > sc-app-root > div:nth-child(2) > section > div > div > div:nth-child(1) > main > ng-component > form > div > section:nth-child(1) > section > sc-personal-info-summary > div:nth-child(2) > div.col-md-4 > div > sc-date-display > span");
 
-        // user data access check
-        alert(userName.innerHTML);
-        alert(userEmail);
-        alert(userPhone.innerHTML);
-        alert(userDOB.innerHTML);
-        alert(userAddress);
+        var user_name = document.querySelector("body > sc-app-root > sc-app-root > div:nth-child(2) > section > div > div > div:nth-child(1) > main > ng-component > form > div > section:nth-child(1) > section > sc-personal-info-summary > div:nth-child(1) > div:nth-child(1) > div > span");
+        user_name = user_name.innerHTML.split(" ");
+        const user_first_name = user_name[0];
+        const user_last_name = user_name[1];
 
-        // Comprise user info into a single row
-        const userInfo = [userName.innerHTML, userEmail, userPhone.innerHTML, userDOB.innerHTML, userAddress];
+        const reference = document.querySelector("body > sc-app-root > sc-app-root > div:nth-child(2) > section > div > div > div:nth-child(1) > main > ng-component > form > div > section:nth-child(1) > section > sc-personal-info-summary > div.row.ng-star-inserted > div:nth-child(6) > div > span");
+
+        const state = document.querySelector("#drivingLicenceSection > sc-driving-licence-summary > div > div:nth-child(2) > div:nth-child(6)");
+        const dv_num = document.querySelector("#drivingLicenceSection > sc-driving-licence-summary > div > div:nth-child(1) > div:nth-child(2)");
+
+        // create payload
+        const payload = {
+            'firstName': user_first_name,
+            'lastName': user_last_name,
+            'dob': user_DOB.innerHTML,
+            'reference': reference.innerHTML,
+            'state': state.innerHTML,
+            'driverLicense': dv_num.innerHTML
+        };
+
+        console.log(payload);
+
+        return payload;
+    }
+
+    function fillMVRChecker(user_info) {
+
     }
 
     // Function to add the recurring service booking options
@@ -41,15 +55,18 @@
 
         if (actionRow && actionRow.children.length == 1) {
             // Create CSV export button
-            const csvButton = document.createElement('button');
-            csvButton.type = 'button';
-            csvButton.className = 'p-element btn btn-link';
-            csvButton.setAttribute('ptooltip', 'Download perosnal information as CSV');
-            csvButton.innerHTML = '<i class="fa fa-download"></i> Export to MVR Checker';
-            csvButton.onclick = function(){convertToCSV()}; 
-            
+            const mvr_button = document.createElement('button');
+            mvr_button.type = 'button';
+            mvr_button.className = 'p-element btn btn-link';
+            mvr_button.setAttribute('ptooltip', 'Transfer data to MVR Checker');
+            mvr_button.innerHTML = '<i class="fa fa-download"></i> Export to MVR Checker';
+            mvr_button.onclick = function(){
+                const user_info = processUserInfo();
+                fillMVRChecker(user_info);
+            };
+
             // Append button to the row div after the first child
-            actionRow.insertBefore(csvButton, actionRow.children[0]);
+            actionRow.insertBefore(mvr_button, actionRow.children[0]);
         }
     }
 
