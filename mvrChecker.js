dc73629e1852a7dd24a1af6f50bda0da578184f2
Harjot Gill <harjotgill2025@gmail.com>
2025-03-23 19:26:17 -0700

    Refactored mvrChecker script to use a state machine design for different states in the form filling process. Name changes for exportLicenseData script for improved readability

--- mvrChecker.js
+++ mvrChecker.js
@@ -10,13 +10,8 @@
 (function () {
     'use strict';
 
-    function login(username, password) {
-        // Fill in the login form and submit
-        document.querySelector("#l-name").value = username;
-        document.querySelector("#l-pass").value = password;
-        // wait for 4 seconds 
-        document.querySelector("#l-btn").click();
-    }
+    const currentUrl = window.location.href;
+    const ORDERFORMURL = "Fill in URL HERE";
 
     const hash = window.location.hash;
 
@@ -26,40 +21,78 @@
             const json = atob(encoded);
             const data = JSON.parse(json);
 
-            console.log("[MVRCheck] Decoded user data:", data);
+            // Save form info to sessionStorage
+            const mvr_form_input = {
+                first_name: data.first_name,
+                middle_name: data.middle_name,
+                last_name: data.last_name,
+                dob: data.dob,
+                license_number: data.license_number,
+                state: data.state,
+                resident_community: data.resident_community,
+            };
 
-            var mvr_form_input = {
-                "first_name": data.first_name,
-                "middle_name": data.middle_name,
-                "last_name": data.last_name,
-                "dob": data.dob,
-                "license_number": data.license_number,
-                "state": data.state,
-                "resident_community": data.resident_community,
-            }
+            var credentials = {
+                username: data.username,
+                password: data.password
+            };
 
-            //TODO: Login with credentials 
+            var script_state = "init";
+            sessionStorage.setItem("script_state", script_state);
 
-            let username_element = document.querySelector("#l-name");
-            let password_element = document.querySelector("#l-pass");
+            sessionStorage.setItem("mvr_form_input", JSON.stringify(mvr_form_input));
+            console.log("[MVRCheck] Decoded user data and saved to session storage:", data);
 
-            if (username_element && password_element) {
-                login(data.username, data.password);
-            }
-
-            // setTimeout(() => {
-            //         console.log("[MVRCheck] Redirecting to MVRCheck order page.");
-            //     }
-            // }, 4000);
-            
-            data = null; // Clear data for security reasons
-
-        } catch (err) {
+        } catch (err) { 
             console.error("Failed to decode base64 from URL:", err);
         }
+    }
 
-        // Clean up URL
-        history.replaceState(null, "", window.location.pathname);
+    console.log("[MVRCheck] Current URL:", currentUrl);
+
+    // -------- LOGIN PAGE --------
+    if (currentUrl.includes("/login")) {
+            // Login
+            const usernameField = document.querySelector("#l-name");
+            const passwordField = document.querySelector("#l-pass");
+            const loginButton = document.querySelector("#l-btn");
+
+            // We do not want to automate this page if there is no session state data. When the mvr export button is clicked, the session state is set to "init"
+            // If we are strolling regularly, we do not want to intervene so we check for the session state
+            var script_state = sessionStorage.getItem("script_state");
+
+            const stored = sessionStorage.getItem("mvr_form_input");
+            if (!stored) return;
+
+            if (usernameField && passwordField && loginButton) {
+                usernameField.value = credentials.username;
+                passwordField.value = credentials.password;
+
+                console.log("[MVRCheck] Logging in...");
+                // Clear the hash from the URL
+                window.location.hash = "";
+                history.replaceState(null, "", window.location.pathname);
+
+                loginButton.click();
+            }        
+    }
+
+    // -------- ORDER PAGE --------
+    else if (currentUrl.includes("is/app")) {
+        console.log("[MVRCheck] On dashboard page.");
+
+        const stored = sessionStorage.getItem("mvr_form_input");
+        if (!stored) return;
+
+        try {
+            //Navigate to the order page
+            sessionStorage.setItem("script_state", "order form");
+            console.log("[MVRCheck] Redirecting to order form...");
+            window.location.href = ORDERFORMURL;
+                        
+        } catch (err) {
+            console.error("Failed to parse stored data:", err);
+        }
     }
 
 })();
