7e3678d617cc47db210f9e5e394d950218c132ed
Harjot Gill <harjotgill2025@gmail.com>
2025-05-04 16:41:40 -0700

    Finalized database schema, remove extra fields renaming for consistency

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -4,76 +4,43 @@ CREATE TABLE bookings (
     created_at TIMESTAMP,
     created_by VARCHAR(255),
     dropoff_datetime TIMESTAMP,
-    pickup_datetime TIMESTAMP, 
-    member_id INTEGER REFERENCES members(id),
-    station_id INTEGER REFERENCES stations(id),
-    station_name VARCHAR(255),
+    pickup_datetime TIMESTAMP,
     status VARCHAR(50),
     total_revenue FLOAT,
     trip_distance FLOAT,
     trip_dropoff_datetime TIMESTAMP,
-    trip_pickup_datetime TIMESTAMP,         
+    trip_pickup_datetime TIMESTAMP,
+    member_id INTEGER REFERENCES members(id),
+    station_id INTEGER REFERENCES stations(id),
     vehicle_id INTEGER REFERENCES vehicles(id)
 );
 
 CREATE TABLE vehicles (
     id INTEGER PRIMARY KEY,
     colour VARCHAR(50),
-    doors INTEGER,
-    fuelType VARCHAR(50),
-    transmission VARCHAR(50),
     year INTEGER,
-    registrationPlate VARCHAR(100),
-    vehicleNumber VARCHAR(100),
-    fullName TEXT,
-    notes TEXT,
-    imageToDisplay TEXT,
-    imageFilename TEXT,
-
-    -- WAITING TO BE DISCUSSED WHETER TO ADD MODEL OR NOT
-    model_id INTEGER,
-    model_name VARCHAR(100),
-    model_fullName TEXT,
-    model_vehicleType_key VARCHAR(50),
-    model_vehicleType_label VARCHAR(50),
-    model_category VARCHAR(50),
-
-
-    manufacturer_id INTEGER,
-    manufacturer_name VARCHAR(100),
+    registration_plate VARCHAR(100),
+    odometer INTEGER,
+    model_full_name VARCHAR(100),
     station_id INTEGER REFERENCES stations(id)
 );
 
-
-CREATE TABLE recentMember(
-    id INTEGER PRIMARY KEY,  
-    fullName VARCHAR(255),
-    createdAt TIMESTAMP,
-    updatedAt TIMESTAMP,
-    email VARCHAR(255),
-    phoneNumber VARCHAR(100),
-    address TEXT,
-    personaInfo_status VARCHAR(50),
-    communityApproval_status VARCHAR(50),
-    drivingLicence_status VARCHAR(50),
+CREATE TABLE recent_members (
+    id INTEGER PRIMARY KEY,
+    full_name VARCHAR(255),
+    created_at TIMESTAMP,
+    driving_licence_status VARCHAR(50)
 );
 
-
 CREATE TABLE stations (
-    id INTEGER PRIMARY KEY,  
+    id INTEGER PRIMARY KEY,
     name VARCHAR(255),
-    status_label VARCHAR(50),
-    community_id INTEGER,
-
+    status_label VARCHAR(50)
 );
 
 CREATE TABLE members (
     id INTEGER PRIMARY KEY,
-    email VARCHAR(255),
     created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    display_name VARCHAR(255),
     first_name VARCHAR(100),
-    last_name VARCHAR(100),
-
+    last_name VARCHAR(100)
 );
